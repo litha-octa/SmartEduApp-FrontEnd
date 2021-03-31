4848706bef5c4de497ea8b45d314a0462027f453
litha <lithaoctadelistia@gmail.com>
2021-03-31 22:31:40 +0700

    regist

--- src/Register.js
+++ src/Register.js
@@ -1,32 +1,36 @@
 import React, { Component } from "react";
 import {Link} from "react-router-dom";
-import "./css/register.css";
+import "./css/register/register.css";
  
 class Register extends Component {
   render() {
     return (
 			
-			<div>
-          <h1>Register</h1>
+			<div id="body-regist">
+          <h1 id="title-regist">Register</h1>
+
           <div id="caption">Username</div>
           <input type="text" id="label-input"/>
+
           <div id="caption">Email</div>
           <input type="text" id="label-input"/>
+
           <div id="caption">Password</div>
-          <input type="text" id="label-input"/>
+          <input type="password" id="label-input"/>
+
           <div id="caption">Confirm Password</div>
-          <input type="text" id="label-input"/>
+          <input type="password" id="label-input"/>
 
           <button type="button" className="btn-regist"> Register</button>
           
           <button type="button" className="btn-googleregist">
                   <img src="assets/logogoogle.png" className="google" alt=" "/>
-                  Register with google
+                  <div id="registwith">Register with google</div>
               </button>
 
               <div id="linkToLogin">
                   Already have account ?
-                    <Link to ="/" className="linkto">Login</Link>
+                    <Link to ="/" className="linktologin">Login</Link>
               </div>
 
   			</div>
