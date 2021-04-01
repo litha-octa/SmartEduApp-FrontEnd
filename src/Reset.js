2789cd077874911ea4ed8384b47e43d634090af9
litha <lithaoctadelistia@gmail.com>
2021-04-01 17:33:49 +0700

    fixin reset

--- src/Reset.js
+++ src/Reset.js
@@ -5,23 +5,23 @@ import "./css/others/reset.css";
 class Reset extends Component {
   render() {
     return (
-    <div>
-        <div className="col" id="box">
+    <div className="container-reset">
+        <div id="box1-reset">
           <Link to="/">
             <img src="assets/Back Icon.png" id="back" alt=" "/>
           </Link>
             <img className="orang" src="assets/orang.png" alt=" "/>
         </div>
-        <div class="col" id="box-2">
-            <h1 id="reset">Reset Password</h1>
-            <h6>Enter your email address linked to .this account.</h6>
-            <h6 className="h6-2">We will send you the verification code to reset your password </h6>
-            <div id="passReset3">Your Email</div>
-            <input type="text" id="namebox"/>
-            <Link to="/Reset2">
-                <button type="button" className="btn-send"><p-send>send</p-send></button>
-            </Link>
-        </div>
+        <div id="box2-reset">
+            <h1 id="reset-caption">Reset Password</h1>
+                <h6 className="rsth6-1">Enter your email address linked to .this account.</h6>
+                    <h6 className="rsth6-2">We will send you the verification code to reset your password </h6>
+                        <div id="passReset1">Your Email</div>
+                            <input type="text" id="inputemail"/>
+                                <Link to="/Reset2">
+                                    <button type="button" className="reset-btn-send">Send</button>
+                                </Link>
+                        </div>
     </div>
     
 
--- src/Reset2.js
+++ src/Reset2.js
@@ -5,20 +5,20 @@ import "./css/others/reset.css";
 class Reset2 extends Component {
   render() {
     return (
-    <div>
-        <div className="col" id="box">
+    <div className="container-reset">
+        <div id="box1-reset">
           <Link to="/Reset">
             <img src="assets/Back Icon.png" id="back" alt=" "/>
           </Link>
             <img className="orang" src="assets/orang2.png" alt=" "/>
         </div>
-        <div class="col" id="box-2">
-            <h1 id="reset">Reset Password</h1>
-            <h6>Enter verification code we just sent to your email address</h6>
+        <div id="box2-reset">
+            <h1 id="reset-caption">Reset Password</h1>
+            <h6 className="rsth6-1">Enter verification code we just sent to your email address</h6>
             <input type="text" id="verCode"/>
-            <h6 className="h6-2">Didn’t receive a code? Resend</h6>
+            <h6 className="rsth6-2">Didn’t receive a code? Resend</h6>
             <Link to="/Reset3">
-            <button type="button" className="btn-send"><p-send>Verify</p-send></button>
+            <button type="button" className="reset-btn-send"><p-send>Verify</p-send></button>
             </Link>
         </div>
     </div>
